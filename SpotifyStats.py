0e6f0e29429576aba12e1b7286eaef0c7ad59e2e
Jack Addison <ja3g20@soton.ac.uk>
2023-06-19 23:33:28 +0100

    added proper database query support - queries are currently broken

--- SpotifyStats.py
+++ SpotifyStats.py
@@ -1,7 +1,14 @@
 import sqlite3, codecs, json
 
+import numpy as np
+
 global c
 global conn
+
+# songs = dict()  # (id : times_played)
+song_ids = dict()  # (artistName, trackName, album) : id
+song_names = dict()  # id : (artistName, trackName, album)
+played_songs = []  # (id, datePlayed, ms_played)
 
 
 def create_new_table():
@@ -17,51 +24,56 @@ def create_new_table():
 
 
 def load_all_time_stats(*filenames):
-
     current_id = 0
     for filename in filenames:
         file = open(filename, encoding="utf8")
         file_json = json.loads(file.read())
         for song in file_json:
-            c.execute('SELECT SongName, Artist, AlbumName FROM SongIDs WHERE SongName = ? AND Artist = ?',
-                      (song["master_metadata_track_name"], song["master_metadata_album_artist_name"]))
-            existing_song = c.fetchall()
-            # print(existing_song)
-
             if song["ms_played"] >= 50000:
-                if not existing_song:
+                if not song_ids.__contains__((song["master_metadata_album_artist_name"],
+                                              song["master_metadata_track_name"],
+                                              song["master_metadata_album_album_name"])):
                     # New song
-                    c.execute('INSERT INTO SongIDs VALUES (?, ?, ?, ?)',
-                              (current_id, song["master_metadata_album_artist_name"],
-                               song["master_metadata_track_name"], song["master_metadata_album_album_name"]))
-                    conn.commit()
-
-                    c.execute('INSERT INTO StreamingHistory VALUES (?, ?, ?)',
-                              (current_id, song["ts"], song["ms_played"]))
-                    conn.commit()
+                    song_ids[(song["master_metadata_album_artist_name"], song["master_metadata_track_name"],
+                              song["master_metadata_album_album_name"])] = current_id
+                    song_names[current_id] = (
+                        song["master_metadata_album_artist_name"], song["master_metadata_track_name"],
+                        song["master_metadata_album_album_name"])
+                    played_songs.append((current_id, song["ts"], song["ms_played"]))
                     current_id += 1
                 else:
                     # Existing song
-                    print("Not new song: " + song["master_metadata_track_name"])
-                    c.execute('SELECT SongID FROM SongIDs WHERE SongName = ? AND Artist = ? AND AlbumName = ?',
-                              (song["master_metadata_album_artist_name"],
-                               song["master_metadata_track_name"], song["master_metadata_album_album_name"]))
-                    song_id = c.fetchone()[0]
+                    played_songs.append((current_id, song["ts"], song["ms_played"]))
 
-                    c.execute('INSERT INTO StreamingHistory VALUES (?, ?, ?)',
-                              (song_id, song["ts"], song["ms_played"]))
-                    conn.commit()
+
+def save_to_db():
+    for play in played_songs:
+        # print(play)
+        c.execute('INSERT INTO StreamingHistory VALUES (?, ?, ?)', (play[0], play[1], play[2]))
+    conn.commit()
+    for song_id in song_ids:
+        # print(song_id)
+        c.execute('INSERT INTO SongIDs VALUES (?, ?, ?, ?)', (song_ids[song_id], song_id[1], song_id[0], song_id[2]))
+    conn.commit()
 
 
 if __name__ == '__main__':
     conn = sqlite3.connect('SpotifyStats.db')
     c = conn.cursor()
     create_new_table()
-    load_all_time_stats('endsong_0.json')
-    # load_all_time_stats('endsong_0.json', 'endsong_1.json', 'endsong_2.json', 'endsong_3.json',
-    #            'endsong_4.json', 'endsong_5.json', 'endsong_6.json', 'endsong_7.json', 'endsong_8.json')
+    # load_all_time_stats('endsong_0.json')
+    load_all_time_stats('endsong_0.json', 'endsong_1.json', 'endsong_2.json', 'endsong_3.json',
+               'endsong_4.json', 'endsong_5.json', 'endsong_6.json', 'endsong_7.json', 'endsong_8.json')
 
-    c.execute('SELECT * FROM SongIDs')
+    save_to_db()
+
+    # Get the top 10 played songs
+    c.execute(
+        'SELECT SongName, Artist, COUNT(*) as `plays` FROM StreamingHistory JOIN SongIDs WHERE StreamingHistory.SongID == SongIDs.SongID GROUP BY SongIDs.SongID ORDER BY `plays` DESC LIMIT 10')
+    print(c.fetchall())
+
+    # Get the top 10 played artists
+    c.execute('SELECT Artist, COUNT(*) as `plays` FROM StreamingHistory JOIN SongIDs WHERE StreamingHistory.SongID == SongIDs.SongID GROUP BY Artist ORDER BY `plays` DESC LIMIT 10')
     print(c.fetchall())
 
     # c.execute('SELECT * FROM StreamingHistory')
